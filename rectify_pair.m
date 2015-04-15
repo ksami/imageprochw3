c99ae72b65e14e08c5b0da4c1c3734299127bb53
ksami <ksami.ihide@gmail.com>
2015-04-15 23:46:06 +0900

    complete rectification

--- rectify_pair.m
+++ rectify_pair.m
@@ -7,3 +7,25 @@ c1 = (A1 * R1) \ (A1 * T1);
 c2 = (A2 * R2) \ (A2 * T2);
 
 % 2. Compute the new rotation matrix %
+r1 = (c1 - c2) / norm(c1 - c2);
+r2 = cross(R1(3,:)', r1);
+r3 = cross(r1, r2);
+Rn = [r1 r2 r3]';
+
+An = A2;
+
+T1n = -Rn * c1;
+T2n = -Rn * c2;
+
+P1n = An * [Rn, T1n];
+P2n = An * [Rn, T2n];
+
+RM1 = (An * Rn) / (A1 * R1);
+RM2 = (An * Rn) / (A2 * R2);
+
+
+%fulfil output variables
+A1n = An;
+A2n = An;
+R1n = Rn;
+R2n = Rn;
